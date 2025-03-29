25ad0afaf491abc7ab7916f5e80393dc43e88ae7
Chakrit <chakrit.t@arise.tech>
2025-03-29 16:53:06 +0700

    database: update, delete, get all products, close database

--- main.go
+++ main.go
@@ -34,6 +34,9 @@ func main() {
 
 	db = sdb
 
+	// เมื่อดึง row เสร็จจะทำ db.Close() = ปิด connection database เพื่อปรัหยัด connection ที่ต่อไปยัง database (connection จะ ไม่สะสม)
+	defer db.Close()
+
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
@@ -50,10 +53,31 @@ func main() {
 	//fmt.Println("Create Successful !")
 
 	// ----- Get Product ----- //
+	//product, err := getProduct(2)
 
-	product, err := getProduct(2)
+	//fmt.Println("Get Successful !", product)
 
-	fmt.Println("Get Successful !", product)
+	// ----- Get All Products ----- //
+
+	products, err := getProducts()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(products)
+
+	// ----- Update Product ----- //
+	//product, err := updateProduct(6, &Product{Name: "New", Price: 555})
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("Update Product Successful !", product)
+
+	// ----- Delete Product ----- //
+	//err = deleteProduct(7)
+	//
+	//fmt.Println("Delete Product Successful !")
+
 }
 
 func createProduct(product *Product) error {
@@ -75,4 +99,57 @@ func getProduct(id int) (Product, error) {
 	}
 
 	return p, nil
+}
+
+func getProducts() ([]Product, error) {
+	rows, err := db.Query("SELECT id, name, price from products")
+	if err != nil {
+		return nil, err
+	}
+
+	var products []Product
+
+	for rows.Next() {
+		var p Product
+		err := rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func updateProduct(id int, product *Product) (Product, error) {
+	var p Product
+	//_, err := db.Exec(
+	//	"UPDATE public.products SET name=$1, price=$2 WHERE id=$3;",
+	//	product.Name,
+	//	product.Price,
+	//	id)
+
+	row := db.QueryRow(
+		"UPDATE public.products SET name=$1, price=$2 WHERE id=$3 RETURNING id, name, price;",
+		product.Name,
+		product.Price,
+		id)
+
+	err := row.Scan(&p.ID, &p.Name, &p.Price)
+	if err != nil {
+		return Product{}, err
+	}
+
+	return p, err
+}
+
+func deleteProduct(id int) error {
+	_, err := db.Exec(
+		"DELETE FROM public.products WHERE id=$1;", id)
+
+	return err
 }
